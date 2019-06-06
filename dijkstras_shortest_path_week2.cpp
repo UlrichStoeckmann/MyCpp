680a3629af461be63313dcd03bcc47d02cbe42cc
Stoeckmann Ulrich (TFEHK3) <ulrich.stoeckmann@continental-corporation.com>
2019-06-06 17:29:36 +0200

    added initialize function, corrected calculation of edge_weight

--- dijkstras_shortest_path_week2.cpp
+++ dijkstras_shortest_path_week2.cpp
@@ -1,16 +1,16 @@
+/*ToDo: 1. alle Variablen und Methoden in die Klasse verschieben
+2. den Dijkstra Algorithmus implementieren */
 #include <iostream>
 #include <ctime>
 #include <random>
 using namespace std;
 
 
-const int MAXV = 10;
-
 double prob()
 {
     return (static_cast<double>(rand())/RAND_MAX);
 }
-double dist()
+double  dist()
 {
     return(1.0+(static_cast<double>((90*rand())/RAND_MAX))/10);
 }
@@ -27,21 +27,24 @@ EdgeNode::EdgeNode(int key, int weight){
     this->key = key;
     this->weight = weight;
     this->next = NULL;
-}
-
-class Graph{
-    public:
-        EdgeNode *edges[MAXV + 1];
-        Graph();
-        ~Graph();
-        void insert_edge(int, int, int);
-        void print();
 };
 
-int main()
+class Dijkstra{
+    public:
+        void initialize();
+        int nodes_amount, MAXV;
+        double edge_density;
+
+};
+void Dijkstra::initialize()
 {
-bool** connected;
-double **distance;
+  bool** connected;
+  double **distance;
+  cout<<"Number of nodes (>1): ";
+  cin>>nodes_amount;
+  cout<<"\nEdge Density (0-1)";
+  cin>>edge_density;
+  MAXV = nodes_amount;
     srand(time(0)); // seed random generator
     connected = new bool*[MAXV];
     distance = new double*[MAXV];
@@ -56,7 +59,7 @@ for(int i=0; i<MAXV;++i)
      for(int j=i; j<MAXV;++j)
      {
         if(i==j) connected[i][j] = false; // no loops
-        else  connected[i][j]=connected[j][i]=(prob()<0.5); //generating the density with distances from 1.0 to 10.0
+        else  connected[i][j]=connected[j][i]=(prob()<edge_density); //generating the density with distances from 1.0 to 10.0
         distance[i][j]= distance[j][i]= connected[i][j] * dist();
      }
 }
@@ -68,6 +71,11 @@ for(int i=0; i<MAXV;++i)
  cout<<"\n";
 }
 
+}
+int main()
+{
+ Dijkstra a;
+ a.initialize();
 
     return 0;
 }
