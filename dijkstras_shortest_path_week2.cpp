caeea3d4b7c51a7e83d0aca96681e8dbf52847d4
UlrichStoeckmann <ulrich.stoeckmann@web.de>
2019-06-10 14:23:13 +0200

    program completed and verified

--- dijkstras_shortest_path_week2.cpp
+++ dijkstras_shortest_path_week2.cpp
@@ -3,9 +3,10 @@
 #include <iostream>
 #include <ctime>
 #include <random>
+#include <conio.h>
 using namespace std;
 
-#define DISTANCE_INIT 9999;
+#define DISTANCE_INIT 9999; //means not connected
 
 double prob()
 {
@@ -36,6 +37,9 @@ class Dijkstra{
         void print(); // print the adjacent matrix of the graph
         void initialize(); // create start values for the dijkstra algo
         void calculate_dist(); //
+        int search_min_node(); //
+        void output(); //present result
+        void display_path(int);
         int nodes_amount, MAXV, source; // number of nodes and start node for finding the shortest path
         double edge_density; // edge density for the graph
         double **connection; // distance between the nodes
@@ -65,7 +69,10 @@ void Dijkstra::create_graph()
         for(int j=i; j<MAXV;++j)
         {
             if(i==j) connection[i][j] = 0; // no loops
-            else  connection[i][j]=connection[j][i]=(prob()<edge_density)*weight(); //generating the density with distances from 1.0 to 10.0
+            else
+            {
+               connection[i][j]=connection[j][i]=(prob()<edge_density)*weight(); //generating the density with distances from 1.0 to 10.0
+            }
 
         }
     }
@@ -83,7 +90,10 @@ void Dijkstra::print()
 
 void Dijkstra::initialize()
 {
-    for(int i=1;i<=MAXV;i++)  // set initial values for all nodes
+    mark = new bool[MAXV];
+    dist = new double[MAXV];
+    predecessor = new int [MAXV];
+    for(int i=0;i<MAXV;i++)  // set initial values for all nodes
     {
         mark[i] = false;
         dist[i] = DISTANCE_INIT ;
@@ -91,23 +101,76 @@ void Dijkstra::initialize()
     }
      dist[source]= 0;
  }
-
+int Dijkstra::search_min_node()
+{
+ int min_dist = DISTANCE_INIT;
+ int closestUnmarkedNode;
+ for(int i=0;i<MAXV;i++)
+    {
+        if((!mark[i]) && ( min_dist >= dist[i]))
+        {
+            min_dist = dist[i];
+            closestUnmarkedNode = i;
+        }
+    }
+ return closestUnmarkedNode;
+}
  void Dijkstra::calculate_dist()
  {
      initialize();
-     int minDistance = DISTANCE_INIT;
      int closestUnmarkedNode;
      int count = 0;
   	 while(count<MAXV)
   	 {
-
+        closestUnmarkedNode = search_min_node();
+        mark[closestUnmarkedNode] = true;
+        for(int i=0;i<MAXV;i++)
+        {
+            if((!mark[i]) && (connection[closestUnmarkedNode][i]>0) ) //
+            {
+                if(dist[i] > dist[closestUnmarkedNode]+connection[closestUnmarkedNode][i])
+                {
+                  dist[i] = dist[closestUnmarkedNode]+connection[closestUnmarkedNode][i]; //found new shortest path to node i
+                  predecessor[i] = closestUnmarkedNode;
+                }
+            }
+  	   }
+  	   count++;
   	 }
  }
+
+void Dijkstra::display_path(int node)
+{
+    if(node == source)
+      cout<<node<<"..";
+    else
+        if(predecessor[node] == -1)
+            cout<<"No path from “<<source<<”to "<<node<<endl;
+        else {
+                display_path(predecessor[node]);
+                cout<<node<<"..";
+             }
+    getch();
+}
+void Dijkstra::output()
+{
+    for(int i=0;i<MAXV;i++)
+        {
+            if(i == source)
+              cout<<source<<".."<<source;
+            else
+                display_path(i);
+            cout<<"->"<<dist[i]<<endl;
+        }
+    getch();
+}
 int main()
 {
  Dijkstra a;
  a.create_graph();
  a.print();
+ a.calculate_dist();
+ a.output();
 
  return 0;
 }
